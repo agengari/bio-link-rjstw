0f0b59453d9d6c56a2638e7dc4dfa5b7ab6898bf
Ageng Ari <agengarinugroho@gmail.com>
2023-05-02 22:52:42 +0700

    Nothing Change

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = () => {
   return (
     <div className='max-w-sm mx-auto grid justify-center gap-4 mt-4 font-bold'>
-      <a href="https://agengari.my.id" className='bg-white rounded-2xl text-center border-amber-200 border-2 py-3 px-20 shadow-md shadow-amber-100'>
+      <a href="https://agengari.my.id" className='bg-white rounded-2xl text-center border-amber-200 border-2 py-3 px-24 shadow-md shadow-amber-100'>
         Website CV 🕵️
       </a>
       <a href="https://agengari.my.id/portfolio/" className='bg-white rounded-2xl text-center border-amber-200 border-2 py-3 shadow-md shadow-amber-100'>
@@ -12,7 +12,6 @@ const Button = () => {
       <a href="https://github.com/agengari" className='bg-white rounded-2xl text-center border-amber-200 border-2 py-3 shadow-md shadow-amber-100'>
         Github 👣
       </a>
-      
     </div>
   )
 }
