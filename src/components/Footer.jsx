382f1c7d5ab400fd4d999407c0fe367f597854de
Ageng Ari <53634443+agengari@users.noreply.github.com>
2023-06-30 08:18:55 +0700

    Update Footer.jsx
    footer link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = ({isSticky}) => {
   return (
     <div className='max-w-sm mx-auto flex justify-center'>
-      <p className={`absolute ${isSticky ? 'sticky mt-4 bottom-4' : 'static bottom-4'}  text-xs`}>Copyright ©️ 2023 Created by <span className='font-bold'>Ageng Ari</span></p>
+      <p className={`absolute ${isSticky ? 'sticky mt-4 bottom-4' : 'static bottom-4'}  text-xs`}>Copyright ©️ 2023 Created by <span className='font-bold'><a href='https://bio.agengari.my.id/'>Ageng Ari</a></span></p>
     </div>
   )
 }
