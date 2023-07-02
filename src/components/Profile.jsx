b447f34e94f82c4268ce2924547fd55f87ce5978
Ageng Ari <agengarinugroho@gmail.com>
2023-07-02 12:14:45 +0700

    Add Verified Icon

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
+import { VscVerifiedFilled } from 'react-icons/vsc'
 
 const Profile = () => {
   return (
-    <div className='max-w-sm mx-auto text-center grid gap-2 -mt-16'>
-      <section className='grid justify-center'>
-        <img src={ require('../assets/img/1675760889282.jpg') } alt="Foto Profil" className='h-28 rounded-full' />
+    <div className="grid max-w-sm gap-2 mx-auto -mt-16 text-center">
+      <section className="grid justify-center">
+        <img src={require('../assets/img/1675760889282.jpg')} alt="Foto Profil" className="rounded-full h-28" />
       </section>
-      <section>
-        <p className='text-xl font-bold mt-4 text-slate-900 dark:text-white'>Ageng Ari .N.</p>
-      </section>
-      <section className='text-sm'>
-        <p className=''>Based in 📌 Yogyakarta, Indonesia</p>
-        <p> 
-          <span>  </span> Interest in Front-End Web Developer.
+      <section className="mx-auto align-middle">
+        <p className="flex mt-4 text-xl font-bold text-slate-900 dark:text-white ">
+          Ageng Ari .N.{' '}
+          <span className="pl-2 text-2xl text-blue-700">
+            <VscVerifiedFilled />
+          </span>
         </p>
       </section>
-      <section className=''>
+      <section className="text-sm">
+        <p className="">Based in 📌 Yogyakarta, Indonesia</p>
+        <p>
+          <span> </span> Interest in Front-End Web Developer.
+        </p>
       </section>
+      <section className=""></section>
     </div>
   )
 }
